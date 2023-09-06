1a57b502917cf74b51c3434bf7aa64790219fcec
jcallisaya <jesus@mail.com>
2023-09-06 19:15:55 -0400

    Adding dependency injection

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -10,7 +10,12 @@ describe('CalculatorService', () => {
         loggerSpy = jasmine.createSpyObj("LoggerService",["log"]);
         
         TestBed.configureTestingModule({
-            providers: [CalculatorService, { provide: LoggerService, useValue: loggerSpy }],
+            providers: [CalculatorService, 
+                { 
+                    provide: LoggerService, 
+                    useValue: loggerSpy 
+                }
+            ],
         });
         
         calculator = TestBed.inject(CalculatorService);
